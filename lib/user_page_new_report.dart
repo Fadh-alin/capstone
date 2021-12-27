e4dea1025db062431b57e04353148cc226a8adc7
Fadh-alin <96581151+Fadh-alin@users.noreply.github.com>
2021-12-27 18:57:23 +0800

    Add files via upload

---
+++ lib/user_page_new_report.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class UserNewReport extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) => Scaffold(
+    body: Center(child: Text('Coding from Azizi', style: TextStyle(fontSize: 60))),
+  );
+}
---
+++ lib/user_page_notification.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class UserNotifications extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) => Scaffold(
+    body: Center(child: Text('Coding from Faiq', style: TextStyle(fontSize: 60))),
+  );
+}
